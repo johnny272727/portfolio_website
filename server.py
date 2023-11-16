ffb18be00975c3024738d35d11b9da3896ffc0fa
Ning Zhang <ningz0227@gmail.com>
2023-11-16 14:41:20 -0800

    added secret key

--- server.py
+++ server.py
@@ -16,7 +16,7 @@ def html_page(page_name):
 @app.route('/submit_form',methods=['GET','POST'])
 def submit_form():
     try:
-        data = [*request.form].to_dict()
+        data = request.form.to_dict()
         save_data_to_csv(data)
         flash('Request has been submitted. I will get in touch with you shortly. Thank You!')
     except Exception as e:
